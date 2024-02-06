68808fefce9ed288de7393abd4161c769a47cb24
mithun30052001 <lmithun01@gmail.com>
2024-02-06 15:44:51 +0530

    Authentication service is created

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http:HttpClient) { }
+  apiurl = 'http://localhost:3000/user'
+
+  GetAll(){
+     return this.http.get(this.apiurl); //gets all users
+  }
+
+  Getbycode(code:any){
+    return this.http.get(this.apiurl+'/'+code); //gets a particular user by code
+  }
+
+  Proceedregister(inputdata:any){
+    return this.http.post(this.apiurl,inputdata); //post the input data to the api endpoint
+  }
+
+  Updateuser(code:any,inputdata:any){
+    return this.http.put(this.apiurl+'/'+code,inputdata); //update the credentials of a user in api endpoint
+  }
+}
