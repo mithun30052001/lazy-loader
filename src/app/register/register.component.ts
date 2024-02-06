abb0a7ee9faefd1144234b251d0fd7ab1d56e994
mithun30052001 <lmithun01@gmail.com>
2024-02-06 15:58:04 +0530

    password validation is changed during signup

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -19,7 +19,7 @@ export class RegisterComponent {
   registerform = this.builder.group({
     id: this.builder.control('',Validators.compose([Validators.required, Validators.minLength(5)])),
     name: this.builder.control('',Validators.required),
-    password: this.builder.control('',Validators.compose([Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')])),
+    password: this.builder.control('',Validators.compose([Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{7,}')])),
     email: this.builder.control('',Validators.compose([Validators.required, Validators.email])),
     gender: this.builder.control('male'),
     role: this.builder.control(''),
