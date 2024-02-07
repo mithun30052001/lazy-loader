8d5cd07d7db764d73980d855abb15f9dcadaffa2
mithun30052001 <lmithun01@gmail.com>
2024-02-07 11:06:14 +0530

    Routing is done for all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { authGuard } from './guard/auth.guard';
+import { UserlistingComponent } from './userlisting/userlisting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent,canActivate: [authGuard()]},
+  {path:'register',component:RegisterComponent},
+  {path:'login',component:LoginComponent},
+  {path:'user',component:UserlistingComponent,canActivate: [authGuard()]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
