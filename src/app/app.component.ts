4174ec534e3a0998b94f7d59d073fd4e4135c801
mithun30052001 <lmithun01@gmail.com>
2024-02-08 12:27:05 +0530

    A navbar for showing navigation links with access based on roles is added to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, DoCheck } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from './service/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,8 @@ import { Router } from '@angular/router';
 export class AppComponent implements DoCheck {
   title = 'angular-dashboard';
   isnavmenurequired = true;
-  constructor(private router:Router){
+  isadminuser = false;
+  constructor(private router:Router, private service: AuthService){
 
   }
 
@@ -18,7 +20,12 @@ export class AppComponent implements DoCheck {
     if(currenturl=='/login' || currenturl =='/register'){
       this.isnavmenurequired = false;
     }else{
-      this.isnavmenurequired =true;
+      this.isnavmenurequired = true;
+    }
+    if(this.service.GetUserrole()==='admin'){
+      this.isadminuser = true;
+    }else{
+      this.isadminuser = false;
     }
   }
 
