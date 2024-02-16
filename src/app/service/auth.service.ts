b0c853ca43195c50cbe0758bd0a6c6daf00fa3fc
mithun30052001 <lmithun01@gmail.com>
2024-02-16 10:39:40 +0530

    Methods for updating the user profile, getting the current user and updating the password of a user are added

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -19,11 +20,16 @@ export class AuthService {
   }
   
   GetAllRole(){
-    return this.http.get(this.roleurl); //gets all users
+    return this.http.get(this.roleurl); //gets all roles
   }
 
   Proceedregister(inputdata:any){
     return this.http.post(this.apiurl,inputdata); //post the input data to the api endpoint
+  }
+
+  Updateregister(code:any,updatedata:any){
+    console.log("Updated data",updatedata);
+    return this.http.put(`${this.apiurl}/${code}`,updatedata);
   }
 
   Updateuser(code:any,inputdata:any){
@@ -36,5 +42,19 @@ export class AuthService {
 
   GetUserrole(){
     return sessionStorage.getItem('userrole') != null? sessionStorage.getItem('userrole')?.toString() : '';
+  }
+  
+  GetCurrentUser():Observable<Object>{
+    const code = sessionStorage.getItem('username');
+    return this.Getbycode(code);
+  }
+
+  UpdatePassword(code: any, newPassword: string): Observable<any> {
+    return this.http.get(`${this.apiurl}/${code}`).pipe(
+      switchMap((user: any) => {
+        user.password = newPassword;
+        return this.http.put(`${this.apiurl}/${code}`, user);
+      })
+    );
   }
 }
