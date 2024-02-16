a56b27c6ac45f3ac97efeb9b2413ea13bcbf0167
mithun30052001 <lmithun01@gmail.com>
2024-02-16 10:41:57 +0530

    Profile page for user is designed with inline edit

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../service/auth.service';
 import { Router } from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-profile',
@@ -9,22 +11,61 @@ import { Router } from '@angular/router';
 })
 export class ProfileComponent {
   user: any; 
+  editedUser: any;
+  editing: boolean = false;
+  profileForm!: FormGroup;
 
-  constructor(private service: AuthService, private router: Router) { }
+  constructor(private builder: FormBuilder,private service: AuthService, private router: Router,private toastr:ToastrService,) { }
 
   ngOnInit(): void {
     this.service.GetCurrentUser().subscribe(data=>{
       this.setCurrentUser(data);
     });
+    this.profileForm = this.builder.group({
+      username: [''],
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.email]],
+      gender: ['', Validators.required],
+      role: ['', Validators.required],
+      isactive: ['']
+    });
   }
 
   setCurrentUser(profile: any){
       this.user = profile;
+      this.profileForm.patchValue({
+        username: this.user.id,
+        name: this.user.name,
+        email: this.user.email,
+        password: this.user.password,
+        gender: this.user.gender,
+        role: this.user.role,
+        isactive: this.user.isactive
+      });
       console.log("Current user",this.user);
   }
   
   changePassword() {
     this.router.navigate(['/change-password']);
   }
+
+  editProfile() {
+    this.editing = true;
+    this.editedUser = { ...this.user };
+  }
   
+  updateProfile() {
+    if (this.profileForm.valid) {
+      this.user = { ...this.profileForm.value };
+      this.editing = false;
+      this.profileForm.disable(); 
+      this.service.Updateregister(this.profileForm.value.username,this.profileForm.value).subscribe(res=>{
+        this.toastr.success('Your profile has been successfully updated');
+      });
+    } else {
+      this.profileForm.markAllAsTouched();
+      this.toastr.warning('Please enter valid data');
+    }
+  }
 }
